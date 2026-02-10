3ec02a4465a5305f95dddf58133d738af6591df4
kaan9898 <kaankarahan98@gmail.com>
2026-02-10 20:50:46 +0200

    passwordEncoder method is commited.

--- src/main/java/com/team4/first_ui_/flow/Security/SecurityConfig.java
+++ src/main/java/com/team4/first_ui_/flow/Security/SecurityConfig.java
@@ -58,4 +58,8 @@ public class SecurityConfig {
 
         return http.build();
     }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
