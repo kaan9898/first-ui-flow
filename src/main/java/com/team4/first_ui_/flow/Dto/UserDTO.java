e69c2b6c8021dd5f9568e959f5533c05db6a0ea0
kaan9898 <kaankarahan98@gmail.com>
2026-02-10 21:15:39 +0200

    UserDTO class is commited.

---
+++ src/main/java/com/team4/first_ui_/flow/Dto/UserDTO.java
@@ -0,0 +1,13 @@
+package com.team4.first_ui_.flow.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private String username;
+    private String password;
+}
