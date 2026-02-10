6c38c5979ee0bd000f19db0b62284b7210b5afb4
g3nri <rodionssokolovs@gmail.com>
2026-02-10 23:28:18 +0200

    Added ItemController to display items list.

---
+++ src/main/java/com/team4/first_ui_/flow/Controller/ItemController.java
@@ -0,0 +1,24 @@
+package com.team4.first_ui_.flow.Controller;
+
+import com.team4.first_ui_.flow.Model.Item;
+import com.team4.first_ui_.flow.Service.ItemService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/items")
+public class ItemController {
+
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping
+    public String getAllItems( Model model) {
+        model.addAttribute("items", itemService.getAllItems());
+        return "items";
+    }
+}
