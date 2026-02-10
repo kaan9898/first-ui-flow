8e256258fb47945aaf6c56f67158385beefc81a4
kaan9898 <kaankarahan98@gmail.com>
2026-02-10 23:22:06 +0200

    Test File is commited and pushed into branch.

---
+++ src/test/java/com/team4/first_ui_/flow/FirstUiFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team4.first_ui_.flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstUiFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
