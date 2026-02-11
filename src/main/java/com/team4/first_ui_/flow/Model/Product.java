246ac459516c6af4bd6aae5a398765510a49731d
kaan9898 <kaankarahan98@gmail.com>
2026-02-11 12:30:32 +0200

    Product.java is updated according to changes to h2 database.

--- src/main/java/com/team4/first_ui_/flow/Model/Product.java
+++ src/main/java/com/team4/first_ui_/flow/Model/Product.java
@@ -1,16 +1,28 @@
 package com.team4.first_ui_.flow.Model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
+@Entity(name = "product")
 @Builder
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int ProductId;
+    @Column(name = "name")
+    @NotBlank(message = "name is required")
     private String ProductName;
+    @Column(name = "category")
+    @NotBlank(message = "Category is required")
     private String ProductCategory;
+    @Column(name = "price")
+    @NotBlank(message = "price is required")
     private Double ProductPrice;
 
     public Product(int ProductId, String ProductName, String ProductCategory, Double ProductPrice) {
