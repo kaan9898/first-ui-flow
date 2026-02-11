b557f9cbab8cd5ac0a684cbbb513b8dff3540553
seduav <sedu.venta@gmail.com>
2026-02-11 14:24:37 +0200

    Added pagination

--- src/main/java/com/team4/first_ui_/flow/Controller/ProductController.java
+++ src/main/java/com/team4/first_ui_/flow/Controller/ProductController.java
@@ -3,6 +3,7 @@ package com.team4.first_ui_.flow.Controller;
 import jakarta.validation.Valid;
 import org.springframework.validation.BindingResult;import com.team4.first_ui_.flow.Model.Product;
 import com.team4.first_ui_.flow.Service.ProductService;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -19,13 +20,21 @@ public class ProductController {
     }
 
     @GetMapping
-    public String getAllProducts( Model model, Principal principal) {
-        model.addAttribute("Products", ProductService.getAllProducts());
+    public String getAllProducts(Model model, Principal principal, @RequestParam(defaultValue = "0") int page) {
+        int pageSize = 10;
+        Page<Product> productPage = ProductService.getProductsPage(page, pageSize);
+
+        model.addAttribute("Products", productPage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", productPage.getTotalPages());
+
         if (principal != null) {
             model.addAttribute("username", principal.getName());
         }
+
         return "Products";
     }
+
     @GetMapping("/new")
     public String showCreateForm( Model model) {
         model.addAttribute("Product", new Product());
