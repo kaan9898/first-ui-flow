3c072919047c907674545b6b96b3a848406735b3
seduav <sedu.venta@gmail.com>
2026-02-11 12:06:14 +0200

    Changed ProductService

--- src/main/java/com/team4/first_ui_/flow/Service/ProductService.java
+++ src/main/java/com/team4/first_ui_/flow/Service/ProductService.java
@@ -5,26 +5,37 @@ import com.team4.first_ui_.flow.Repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
 
     private final ProductRepository ProductRepository;
 
-    public ProductService(ProductRepository ProductRepository) {
-        this.ProductRepository = ProductRepository;
+    public ProductService(ProductRepository productRepository) {
+        this.ProductRepository = productRepository;
+    }
+
+    private Product mapToEntity(Product product) {
+        return Product.builder()
+                .productId(product.getProductId())
+                .productName(product.getProductName())
+                .productCategory(product.getProductCategory())
+                .productPrice(product.getProductPrice())
+                .build();
     }
 
     public List<Product> getAllProducts() {
         return ProductRepository.findAll();
     }
 
-    public void saveProduct(Product Product) {
-        ProductRepository.save(Product);
+    public void saveProduct(Product product) {
+        Product entity = mapToEntity(product);
+        ProductRepository.save(entity);
     }
 
     public void deleteProductById(Long id) {
-        ProductRepository.deleteById(id);
+        ProductRepository.deleteById(id.intValue());
     }
 
 }
